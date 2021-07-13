f33b7ca47c7ff114450b99859eeb9bd0a2bccaee
Dexter Ramos <blitzdex27@gmail.com>
2021-07-13 09:56:33 +0800

    Mitigate client screenshot file creation bug

--- client/src/controllers/takeScnShot.js
+++ client/src/controllers/takeScnShot.js
@@ -4,15 +4,11 @@ import fs from 'fs';
 
 const isProd = path.parse(process.argv[1]).name === 'main';
 
-const imgPath = path.resolve(
-  __dirname,
-  `../${isProd ? '' : '..'}`,
-  'scnshot.jpg'
-);
+const imgPath = path.resolve(__dirname, `../${isProd ? '' : '..'}`, 'scn.jpg');
 
 export default (req, res) => {
   console.log('taking scnshot?');
-  const takeScnShot = cp.spawn('nircmd.exe', ['savescreenshot', 'scnshot.jpg']);
+  const takeScnShot = cp.spawn('nircmd.exe', ['savescreenshot', 'scn.jpg']);
 
   takeScnShot.on('close', () => {
     const file = fs.createReadStream(imgPath);
